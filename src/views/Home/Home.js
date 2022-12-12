ddbf9c3db5a173c0cd056587f3571bbc311da8b8
Tomás Quintal <tomas.quintal@gmail.com>
2022-12-12 15:20:38 +0000

    bug fix: firefox images fix

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -76,7 +76,7 @@ function Home() {
                 <div className={classes['books-container']}>
                     {Books.sort((a, b) => a.id - b.id).map(book => book.id <= 4 &&
                         <div key={book.id} className={classes['book-container']}>
-                            <img src={book.image} alt={book.title} />
+                            <img src={book.image} alt={book.title} className={classes['book-cover']} />
                             <div className={classes['book-description']}>
                                 <p className={classes['book-title']}>{book.title}</p>
                                 <p className={classes['book-author']}>{book.author}</p>
